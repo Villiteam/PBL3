5eb14761fe23741c760d423cae0f2cf1091ce79d
HQuoan <huyvodtan@gmail.com>
2023-04-12 00:04:50 +0700

    toàn bộ code cũ

---
+++ PBL3/Controllers/CategoryController.cs
@@ -0,0 +1,72 @@
+﻿using PBL3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PBL3.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        private pbl3Entities db = new pbl3Entities();
+        public ActionResult Index()
+        {
+            var ds = db.Categories.OrderByDescending(m => m.CatID).ToList();
+            return View(ds);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Category model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //add vao csdl
+                    db.Categories.Add(model);
+                    // luu lai thay doi
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = "Lưu dữ liệu thất bại. Vui lòng thử lại!";
+                return View(model);
+            }
+
+
+        }
+        public ActionResult Edit(int id)
+        {
+            var model = db.Categories.Find(id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category model)
+        {
+            var up = db.Categories.Find(model.CatID);
+            up.CatName = model.CatName;
+            up.Status = model.Status;
+            up.ParentID = model.ParentID;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Delete(int id)
+        {
+            var del = db.Categories.Find(id);
+            db.Categories.Remove(del);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ PBL3/Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using PagedList;
+using PBL3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace PBL3.Controllers
+{
+    public class UserController : Controller
+    {
+        private pbl3Entities db = new pbl3Entities();
+        // GET: User
+        //  [AdminAuthorize(Role = new string[] { "Manager" })]
+        public ActionResult Index(int? page)
+        {
+            var pageSize = 5;
+            if (page == null)
+            {
+                page = 1;
+            }
+            ViewBag.Page = (page - 1) * pageSize + 1;
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var ds = db.Users.OrderByDescending(m => m.UserID).ToPagedList(pageIndex, pageSize);
+            return View(ds);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(User model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    model.CreatedDate = DateTime.Now;
+                    var passwrHash = Crypto.HashPassword(model.Password);
+                    model.Password= passwrHash;
+                    //add vao csdl
+                    db.Users.Add(model);
+                    // luu lai thay doi
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = "Lưu dữ liệu thất bại. Vui lòng nhập đầy đủ dữ liệu!";
+                return View(model);
+            }
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var model = db.Users.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User model)
+        {
+            var update = db.Users.Find(model.UserID);
+            update.UserName = model.UserName;
+            if (update.Password == model.Password)
+            {
+                update.Password = model.Password;
+            }
+            else
+            {
+                var passwrHash = Crypto.HashPassword(model.Password);
+                update.Password = passwrHash;
+            }
+            update.Email = model.Email;
+            update.Role = model.Role;
+            update.Status = model.Status;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var del = db.Users.Find(id);
+            db.Users.Remove(del);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
