4dd82db59fc1a912fc5aeef1c7f6f726a6249b63
HQuoan <huyvodtan@gmail.com>
2023-05-04 22:58:12 +0700

    thêm phần thống kê

---
+++ PBL3/Areas/Admin/Controllers/StatisticalController.cs
@@ -0,0 +1,64 @@
+﻿using PBL3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PBL3.Areas.Admin.Controllers
+{
+    public class StatisticalController : Controller
+    {
+        // GET: Admin/Statistical
+        private pbl3Entities db = new pbl3Entities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult GetStatistical(string fromDate, string toDate)
+        {
+            var query = from o in db.Orders
+                        join od in db.OrderDetails
+                        on o.OrderID equals od.OrderID
+                        join p in db.Products
+                        on od.ProductID equals p.ProductID
+                        select new
+                        {
+                            CreateDate = o.OrderDate,
+                            Quantity = od.Quantity,
+                            Price = od.Price,
+                            OriginalPrice = p.OriginalPrice
+                        };
+
+            if (!string.IsNullOrEmpty(fromDate))
+            {
+                DateTime startDate = DateTime.ParseExact(fromDate, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                query = query.Where(x => x.CreateDate >= startDate);
+            }
+            if (!string.IsNullOrEmpty(toDate))
+            {
+                DateTime endDate = DateTime.ParseExact(toDate, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                query = query.Where(x => x.CreateDate <= endDate);
+            }
+
+            var result = query.GroupBy(x => DbFunctions.TruncateTime(x.CreateDate)).Select(x => new
+            {
+                Date = x.Key.Value,
+                TotalBuy = x.Sum(y => y.Quantity * y.OriginalPrice),
+                TotalSell = x.Sum(y => y.Quantity * y.Price)
+            }).Select(x => new
+            {
+                Date = x.Date,
+                DoanhThu = x.TotalSell,
+                LoiNhuan = x.TotalSell - x.TotalBuy
+            });
+
+
+            return Json(new { Data = result }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
